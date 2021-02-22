06efd10dadbfbf7d70a9622665ba2f5302cd3b99
cylon56 <michaelnlewellen@gmail.com>
2021-02-22 13:15:17 -0600

    first commit

---
+++ src/js/app.vue
@@ -0,0 +1,25 @@
+<template lang="pug">
+  <div>
+    <Form v-on:nameChange="onNameChange"></Form>
+  </div>
+</template>
+
+<script>
+import Form from './components/form'
+export default {
+  components: {
+    Form
+  },
+  methods: {
+    onNameChange(nameInput) {
+      console.log(nameInput)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.container {
+  color: green;
+}
+</style>
---
+++ src/js/components/form.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <input v-model="message" placeholder='Full Name'>
+        <p>Name is: {{ capitalizeName }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: `Form`,
+    data() {
+        return {
+            message: ``
+        }
+    },
+    computed: {
+        capitalizeName() {
+            return this.message.split(` `).map(s => s.charAt(0).toUpperCase() + s.slice(1)).join(` `)
+        }
+    },
+    methods: {
+        onNameInput(event) {
+            this.$emit('nameChange', event.input.value)
+        }
+    }
+}
+</script>
